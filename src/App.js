e51e8b6f4446778dcac67e1724af4c970288925f
Gabriel Faro <faro@Mac-mini-de-Gabriel.local>
2018-03-13 18:52:59 -0300

    main page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,115 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Order from "./components/Order";
-import Input from "./components/Input";
+//import Input from "./components/Input";
 import { Container, Row, Col } from "react-grid-system";
 import "./App.css";
+import CircularProgress from "material-ui/CircularProgress";
+import Paper from "material-ui/Paper";
+
+const style = {
+  borderRadius: 10,
+  height: 80,
+  width: 200,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "white",
+  margin: 20
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      values: []
+      values: ["123"],
+      progress: -1,
+      currentValue: "",
+      hasValues: true
     };
   }
+
+  componentWillMount() {
+    if (this.state.values.length === 0) {
+      this.setState({ hasValues: false });
+    }
+    this.setupKey();
+  }
+
+  setupKey = event => {
+    document.onkeyup = this.handlerKeyUP;
+  };
+
+  handlerKeyUP = event => {
+    if (event.keyCode > 47 && event.keyCode < 58) {
+      let char = String(event.keyCode - 48);
+      this.setState({ currentValue: this.state.currentValue + char });
+      this.setupInterval();
+    }
+  };
+
+  setupInterval = () => {
+    clearInterval(this.interval);
+    this.setState({ progress: 0 }, () => {
+      this.interval = setInterval(() => {
+        let currentProgress = this.state.progress;
+        if (currentProgress >= 120) {
+          this.addOrder();
+        } else {
+          this.setState({ progress: currentProgress + 4 });
+        }
+      }, 40);
+    });
+  };
+
+  addOrder = () => {
+    clearInterval(this.interval);
+    this.setState({
+      values: this.state.values.concat(this.state.currentValue),
+      progress: -1,
+      currentValue: ""
+    });
+  };
 
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
+        <div className="App" tabIndex={0} onKeyPress={this.handlerKeyUP}>
           <header className="App-header">
             <h1 className="App-title">Order Displayer</h1>
           </header>
 
           <div className="Content">
+            <Paper style={style}>
+              <div style={style}>
+                <span
+                  style={{ fontSize: 32, fontWeight: 500, paddingRight: 10 }}
+                >
+                  {this.state.currentValue}
+                </span>
+                {this.state.mounted
+                  ? <span>OK</span>
+                  : this.state.progress > -1 &&
+                    <CircularProgress
+                      size={32}
+                      mode="determinate"
+                      value={this.state.progress}
+                    />}
+              </div>
+            </Paper>
             <Container>
-              <Row>
-                {this.state.values.map((item, index) =>
-                  <Col key={index} xs={6} md={3}>
-                    <Order orderNumber={item} />
-                  </Col>
-                )}
-              </Row>
+              {this.state.hasValues
+                ? <Row>
+                    {this.state.values.map((item, index) =>
+                      <Col key={index} xs={4} md={2}>
+                        <Order orderNumber={item} />
+                      </Col>
+                    )}
+                  </Row>
+                : <div>
+                    <p className="errorMessage">Nada a ser exibido.</p>
+                  </div>}
             </Container>
-            <Input />
           </div>
         </div>
       </MuiThemeProvider>
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Paper from "material-ui/Paper";
 
 const style = {
   borderRadius: 10,
-  paddingTop:'11%',
-  height: 65,
-  width: "85%",
-  marginTop: "20%",
-  marginBottom: "20%",
-  marginRight: "20%",
-  textAlign: "center",  
-  display: "inline-block",
-  backgroundColor: 'white'
+  height: 60,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "white",
+  padding: 20,
+  margin: 10
 };
 
 export default class Order extends Component {
-
-  state = { 
-    orderNumber: this.props.orderNumber
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div>
-        <Paper
-          style={style}
-          zDepth={1}
-        > 
-          {this.state.orderNumber}
-        </Paper>        
+        <Paper style={style} zDepth={1}>
+          <h2>
+            {this.props.orderNumber}
+          </h2>
+        </Paper>
       </div>
     );
   }
